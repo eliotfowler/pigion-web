8076296f97da2771da76f3b8a368b34fb13c6836
Eliot Fowler <eliot@eliotfowler.com>
2014-09-23 22:54:48 -0400

    sub header in place

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('pigionWebApp')
-    .controller('DashboardCtrl', function ($scope) {
 
+    .controller('DashboardCtrl', function ($scope) {
+        $scope.files = ['one', 'two', 'three', 'four'];
+        $scope.totalFileSize = 200;
+        $scope.userAllTimeFiles = 32;
     });
