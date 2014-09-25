cb8aa6099f3632f071afa527c2c0f582d1dee2da
Eliot Fowler <eliot@eliotfowler.com>
2014-09-24 20:39:13 -0400

    adding user uploaded files

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -3,7 +3,46 @@
 angular.module('pigionWebApp')
 
     .controller('DashboardCtrl', function ($scope) {
-        $scope.files = ['one', 'two', 'three', 'four'];
+        $scope.files = [
+            {
+                name: 'Pigion',
+                extension: 'css',
+                numDownloads: 0,
+                maxDownloads: -1,
+                password: false,
+                expirationDate: '2014-09-30T13:30:00',
+                size: 28
+            },
+            {
+                name: 'Swift',
+                extension: 'png',
+                numDownloads: 5,
+                maxDownloads: 10,
+                password: true,
+                expirationDate: '2014-09-30T13:30:00',
+                size: 28
+            },
+            {
+                name: 'Pigion',
+                extension: 'css',
+                numDownloads: 1,
+                maxDownloads: 1,
+                password: true,
+                expirationDate: '2014-09-30T13:30:00',
+                size: 56
+            },
+            {
+                name: 'Saxon Whitetail blk white superstar',
+                extension: 'jpg',
+                numDownloads: 0,
+                maxDownloads: -1,
+                password: true,
+                expirationDate: '2014-09-30T13:30:00',
+                size: 82
+            }
+        ];
+
+
         $scope.totalFileSize = 200;
         $scope.userAllTimeFiles = 32;
     });
