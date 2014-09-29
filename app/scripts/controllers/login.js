d0938fb70a30a556b0304c4b530800d4e4ccd928
Eliot Fowler <eliot@eliotfowler.com>
2014-09-29 00:42:48 -0400

    using user service to save the token

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('pigionWebApp')
-    .controller('LoginCtrl', function ($scope, Restangular, $location) {
+    .controller('LoginCtrl', function ($scope, Restangular, $location, UserService) {
         $scope.login = function login() {
             Restangular.all('auth').all('api').all('authenticate').all('userpass').post({
                 username: $scope.username,
                 password: $scope.password
             }).then(function(response) {
-                console.log('response.token', response.token);
                 Restangular.configuration.defaultHeaders["X-Auth-Token"] = response.token;
+                UserService.saveUserToken(response.token);
                 $location.path("/dashboard");
             }, function(response) {
                 console.log("There was an error saving", response);
