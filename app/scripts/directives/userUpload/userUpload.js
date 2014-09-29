de51223fcb863ece996777c78afae5402f79da2d
Eliot Fowler <eliot@eliotfowler.com>
2014-09-28 20:23:04 -0400

    working on the progress bar background, getting close

--- app/scripts/directives/userUpload/userUpload.js
+++ app/scripts/directives/userUpload/userUpload.js
@@ -5,15 +5,36 @@ angular.module('pigionWebApp').directive('userUpload', function () {
         scope: {
             'file': '=file'
         },
-        controller: function($scope) {
-            var numPasswordBubbles = 5;
-            $scope.getNumPasswordBubbles = function() {
-                return new Array(numPasswordBubbles);
-            }
-
+        controller: function($scope, $timeout) {
             $scope.date = new Date($scope.file.expirationDate);
+            $scope.newFile = Math.random()<.5;
+
+            $scope.counter =  !!$scope.newFile ? 0 : 100;
+            $scope.doneUploading = !$scope.newFile;
+            $scope.max = 100;
+
+            $scope.countdown = function() {
+                $timeout(function() {
+                    $scope.dynamic = $scope.counter;
+                    $scope.percentUploaded = $scope.dynamic;
+                    $scope.counter++;
+                    if($scope.dynamic < $scope.max) {
+                        $scope.countdown();
+                    } else {
+                        console.log('here');
+                        $scope.doneUploading = true;
+                    }
+
+                }, 1000);
+            };
+
+            $scope.countdown();
+
         },
         link: function postLink(scope, element, attrs) {
+            if (attrs.newFile) {
+                // Do stuff based on new file here
+            }
         }
     };
 });
