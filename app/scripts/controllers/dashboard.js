497abac791ae91ee81a4e82936498338312a9b8a
Eliot Fowler <eliot@eliotfowler.com>
2014-09-29 00:42:08 -0400

    adding a size filter and user token saving service

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -2,7 +2,8 @@
 
 angular.module('pigionWebApp')
 
-    .controller('DashboardCtrl', function ($scope, hotkeys, $upload) {
+    .controller('DashboardCtrl', function ($scope, hotkeys, $upload, Restangular, UserService) {
+        console.log('token', UserService.getUserToken());
         $scope.files = [
             {
                 name: 'Pigion',
@@ -11,7 +12,8 @@ angular.module('pigionWebApp')
                 maxDownloads: -1,
                 password: false,
                 expirationDate: '2014-09-30T13:30:00',
-                size: 28
+                size: 28000,
+                newFile: false
             },
             {
                 name: 'Swift',
@@ -20,7 +22,8 @@ angular.module('pigionWebApp')
                 maxDownloads: 10,
                 password: true,
                 expirationDate: '2014-09-30T13:30:00',
-                size: 28
+                size: 28000,
+                newFile: false
             },
             {
                 name: 'Pigion',
@@ -29,7 +32,8 @@ angular.module('pigionWebApp')
                 maxDownloads: 1,
                 password: false,
                 expirationDate: '2014-09-30T13:30:00',
-                size: 56
+                size: 56000,
+                newFile: false
             },
             {
                 name: 'Saxon Whitetail blk white superstar',
@@ -38,46 +42,71 @@ angular.module('pigionWebApp')
                 maxDownloads: -1,
                 password: true,
                 expirationDate: '2014-09-30T13:30:00',
-                size: 82
+                size: 82000,
+                newFile: false
             }
         ];
+
+        function getSortFunction(fieldName) {
+            return function(file1, file2) {
+                return new Date(file1[fieldName]) > new Date(file2[fieldName]);
+            }
+        }
+
+        $scope.files.sort(getSortFunction('expirationDate'));
 
         $scope.toggleCheatSheet = hotkeys.toggleCheatSheet;
 
         $scope.totalFileSize = 200;
         $scope.userAllTimeFiles = 32;
 
+        var guid = (function() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+            return function() {
+                return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                    s4() + '-' + s4() + s4() + s4();
+            };
+        })();
+
         $scope.onFileSelect = function($files) {
             //$files: an array of files selected, each file has name, size, and type.
             for (var i = 0; i < $files.length; i++) {
                 var file = $files[i];
-                console.log(file);
-//                $scope.upload = $upload.upload({
-//                    url: 'server/upload/url', //upload.php script, node.js route, or servlet url
-//                    //method: 'POST' or 'PUT',
-//                    //headers: {'header-key': 'header-value'},
-//                    //withCredentials: true,
-//                    data: {myObj: $scope.myModelObj},
-//                    file: file, // or list of files ($files) for html5 only
-//                    //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-//                    // customize file formData name ('Content-Disposition'), server side file variable name.
-//                    //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file'
-//                    // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-//                    //formDataAppender: function(formData, key, val){}
-//                }).progress(function(evt) {
-//                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-//                }).success(function(data, status, headers, config) {
-//                    // file is uploaded successfully
-//                    console.log(data);
-//                });
-                //.error(...)
-                //.then(success, error, progress);
-                // access or attach event listeners to the underlying XMLHttpRequest.
-                //.xhr(function(xhr){xhr.upload.addEventListener(...)})
+                var newGuid = guid();
+                var expDate = new Date();
+                var numberOfDaysToAdd = 5;
+                expDate.setDate(expDate.getDate() + numberOfDaysToAdd);
+                var newFile = {
+                    name: file.name.substr(0, file.name.lastIndexOf('.')),
+                    extension: file.name.substr(file.name.lastIndexOf('.') + 1),
+                    numDownloads: 0,
+                    maxDownloads: -1,
+                    password: false,
+                    expirationDate: expDate.toISOString(),
+                    size: file.size,
+                    newFile: true,
+                    guid: newGuid
+                };
+
+                $scope.files.unshift(newFile);
+                $scope.upload = $upload.upload({
+                    url: 'http://localhost:9000/files/upload', //upload.php script, node.js route, or servlet url
+                    //method: 'POST' or 'PUT',
+                    headers: {'X-Auth-Token': UserService.getUserToken()},
+                    file: file
+                }).progress(function (evt) {
+                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                    $scope.$broadcast('fileUploadPercent-' + newGuid, parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function (data, status, headers, config) {
+                    // file is uploaded successfully
+                    console.log(data);
+                }).error(function (result) {
+                    console.log('error', result);
+                });
             }
-            /* alternative way of uploading, send the file binary with the file's content-type.
-             Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed.
-             It could also be used to monitor the progress of a normal http post/put request with large data*/
-            // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
         };
     });
