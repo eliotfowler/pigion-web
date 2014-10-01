c68a46061af8a1cc094515fcc1c294b61e1540ab
Eliot Fowler <eliot@eliotfowler.com>
2014-10-01 19:27:15 -0400

    terrible things have to happen for this to work

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
- 
-var logger = require('morgan');
 
-app.use(logger);
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(express.static(__dirname + '/dist'))
+
 app.listen(process.env.PORT || 5000);
+
