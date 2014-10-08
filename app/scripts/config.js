83039670ac55b5ffe57c451d37e75d3f65b6cc66
Andrew Acree <son.of.robins@gmail.com>
2014-10-07 22:52:41 -0400

    You told me to do this

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'https://pigion.herokuapp.com'})
+.constant('ENV', {name:'development',apiEndpoint:'http://localhost:9000'})
 
 ;
