165fb60b9788d794ebefd513aabcdc12f473fc8e
Eliot Fowler <eliot@eliotfowler.com>
2014-10-15 21:07:50 -0400

    heroku fix

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/build'));
 
 app.all('/*', function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
-    res.sendfile('dist/index.html', { root: __dirname });
+    res.sendfile('build/index.html', { root: __dirname });
 });
 
 app.listen(process.env.PORT || 5000);
